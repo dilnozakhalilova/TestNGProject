73e08d8d2e56c961bd7e69a5c9e678b105519168
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-02-03 21:17:36 -0500

    LoginValidation

--- src/com/exelenter/class02/_01_LogInValidationTest.java
+++ src/com/exelenter/class02/_01_LogInValidationTest.java
@@ -1,11 +1,23 @@
 package com.exelenter.class02;
 
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class _01_LogInValidationTest {
+    @Test
+    void titleValidation() {
 
+    }
 
+    @Test
+    void logoValidation() {
 
+    }
 
+    @Test
+    void validLoginTest() {
 
+    }
 
 
 }
