bad2d032decda00687f4cdcf92318a2c2c8c9f84
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-02-06 22:04:36 -0500

    softAssertAll()

--- src/com/exelenter/class02/_05_SoftAssertDemo.java
+++ src/com/exelenter/class02/_05_SoftAssertDemo.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.DashboardPage;
 import pages.LoginPage;
 import utils.BaseClass;
@@ -25,12 +26,14 @@ public class _05_SoftAssertDemo extends BaseClass {
     @Test
     void softAssertionTest() {
         // First: Verify Logo is displayed
-
         LoginPage login = new LoginPage();
         boolean displayed = login.homepageLogo.isDisplayed();
         displayed=false;
-        Assert.assertTrue(displayed, "Homepage Logo is not displayed");  // Validating logo
+        //Assert.assertTrue(displayed, "Homepage Logo is not displayed");  // Validating logo
 
+        // Soft Assert( if soft Assert fails, remaining test steps will continue)
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(displayed, "Homepage Logo is not displayed");
 
         // Second: Login to the website
 
@@ -41,6 +44,11 @@ public class _05_SoftAssertDemo extends BaseClass {
         Assert.assertEquals(dashboardPage.welcome.getText(), "Welcome Admin", "'Welcome Admin' text is not correct");  // Validating logo
 
         System.out.println("Using Hard Assert if previous test assertion failed, this line will not print");
+
+        // assertAll() method MUST be used at the end of soft-assert test. This will check ALL assertions, if one fails all test fails.
+        // but if you forget it, if one passes, entire @Test passes
+
+        softAssert.assertAll();
     }
 
 
