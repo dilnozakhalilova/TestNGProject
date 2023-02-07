eaf531b8e36d3d994ba7080389dc02ab21e032ba
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-02-06 20:14:27 -0500

    Task

--- src/com/exelenter/class02/_02_AssertDemo.java
+++ src/com/exelenter/class02/_02_AssertDemo.java
@@ -5,6 +5,22 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utils.BaseClass;
 
+/**
+ * TC 01: Login Validation Test (Happy Path)
+ * Open chrome browser
+ * Go to https://exelentersdet.com
+ * Enter valid username and password
+ * Click on login button
+ * Verify login is successful
+ * Close the browser
+ *
+ * TC 02: Login Validation Test (Negative Test)
+ * Open chrome browser
+ * Go to http://exelentersdet.com
+ * Enter valid username
+ * Leave password field empty
+ * Verify error message with text “Password cannot be empty” is displayed.
+ */
 public class _02_AssertDemo extends BaseClass {
    @BeforeMethod
    void openBrowser(){
