9e13935ad18c4ae85be7043a200c26d3cd46a9c7
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-02-06 21:04:48 -0500

    negative testing

---
+++ src/com/exelenter/class02/_03_TitleAndLogoValidation.java
@@ -0,0 +1,42 @@
+package com.exelenter.class02;
+
+import org.bouncycastle.jcajce.provider.symmetric.ARC4;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import utils.BaseClass;
+
+public class _03_TitleAndLogoValidation extends BaseClass {
+    @BeforeMethod
+    void openBrowser() {
+        setUp();
+
+    }
+
+    @AfterMethod
+    void quitBrowser() {
+        tearDown();
+    }
+
+@Test
+    void titleValidation(){
+    String expectedTitle = "Exelenter Project";
+    Assert.assertEquals(expectedTitle,driver.getTitle(),"Title is not verified");
+}
+
+    @Test(dependsOnMethods = "titleValidation")
+    void logoValidation(){
+        var loginPage = new LoginPage();
+        boolean actualLogoDisplayed = loginPage.homepageLogo.isDisplayed();
+        Assert.assertTrue(actualLogoDisplayed, "Logo is not Displayed");
+
+
+}
+
+
+
+
+
+}
