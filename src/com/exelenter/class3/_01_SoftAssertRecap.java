2622506bbafad02e0990d36fba48297230f6d6ee
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-02-07 21:36:43 -0500

    adding userstory

--- src/com/exelenter/class3/_01_SoftAssertRecap.java
+++ src/com/exelenter/class3/_01_SoftAssertRecap.java
@@ -8,17 +8,25 @@ import utils.BaseClass;
 public class _01_SoftAssertRecap extends BaseClass {
 
     @Test
-    public void testCase(){
+    public void testCase(){    // Use only one assert at a time
+
+        // Example of hard Assert
 //       System.out.println("Before Assert");
 //       Assert.assertTrue(false);
-//       System.out.println("After Assert");
+//       System.out.println("After Assert");    // This will not print if line before fails.
 
+
+
+        // Example of Soft Assert
 
         System.out.println("Before Soft Assert");
         SoftAssert softAssert= new SoftAssert();
         softAssert.assertTrue(false);
         System.out.println("After Soft Assert");// This line will print, even if Soft assert before it fails.
         softAssert.assertAll();
+        System.out.println("ALL test steps passed.");  // This line will only print if all steps  pass, otherwise it will not print
     }
 
+    // Remember: Use only one Assertion per test case(@Test method)
+    // I am using multiple only because of hard and soft assert Demonstration
 }
--- src/test/LoginTest.java
+++ src/test/LoginTest.java
@@ -1,35 +1,50 @@
 package test;
 
 
-import pages.LoginPageWithoutPageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
 import pages.LoginPage;
 import utils.BaseClass;
 import utils.ConfigsReader;
 
+/**
+ * User stories
+ * US 16457: As an Admin user I should be able to login to the admin page using valid login credentials.
+ * US 23869: As a User, I should be able to login to the dashbaord page using valid login credentials.
+ * US 83764: As a Business User, here goes your request...
+ */
+
 public class LoginTest extends BaseClass {
-    public static void main(String[] args) {
-        setUp();
-
-        // Note: Make sure to comment out one before using the other.
-
-//        // Without PageFactory
-
-
-        var loginPage = new LoginPageWithoutPageFactory();
-//        sendText(loginPage.username, ConfigsReader.getProperties("username"));
-//        sendText(loginPage.password, ConfigsReader.getProperties("password"));
-//        clickButWaitForClickability(loginPage.loginBtn);
-
-
-        // With PageFactory
-
+    @Test
+    public void validAdminLogin() {
         var login = new LoginPage();
         sendText(login.username, ConfigsReader.getProperties("username"));
         sendText(login.password, ConfigsReader.getProperties("password"));
         clickButWaitForClickability(login.loginBtn);
 
+        var dashboard = new DashboardPage();
+        String expectedText = "Welcome Admin";
+        String actualText = dashboard.welcome.getText();
+        Assert.assertEquals(expectedText, actualText, "'Welcome Admin' text is incorrect");
 
-        tearDown();
+    }
+
+    @Test
+    public void validUserInvalidPassword() {
+        String invalidPassword = "Pass1234";
+        String expectedErrorMessage = "Invalid credentials";
+        LoginPage loginPage = new LoginPage();
+        sendText(loginPage.username, ConfigsReader.getProperties("username"));   // Valid username
+        sendText(loginPage.password, invalidPassword);                                  // Invalid password
+        clickButWaitForClickability(loginPage.loginBtn);
+        Assert.assertEquals(loginPage.loginErrorMessage,expectedErrorMessage,"error message is incorrect. Test failed.");
+    }
+
+    @Test
+    public void validUserAnEmptyPassword() {
+
+
     }
 
 
