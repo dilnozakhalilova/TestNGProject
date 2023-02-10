d0a5e9e418a108073d05a360db6e162c7f25d0e6
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-02-09 20:55:06 -0500

    Cleaning code

--- src/test/AddEmployeeTest.java
+++ src/test/AddEmployeeTest.java
@@ -22,13 +22,8 @@ public class AddEmployeeTest extends BaseClass {
 
     @Test
     void addEmployeeTest() {
-        //var loginPage = new LoginPage();    // now using PageInitializer in  utils
         loginPage.loginToWebsite(ConfigsReader.getProperties("username"), ConfigsReader.getProperties("password"));
-
-        //var pimPage = new PIMPage(); // now using PageInitializer in  utils
         pimPage.navigateToAddEmployee();
-
-        //var addEmployeePage = new AddEmployeePage(); // now using PageInitializer in  utils
         System.out.println("New employee ID: " + addEmployeePage.employeeId.getAttribute("value"));
         addEmployeePage.addEmployee(ConfigsReader.getProperties("empFirstName"),ConfigsReader.getProperties("empLastName"),ConfigsReader.getProperties("filePath")); // This method will add a new employee
 
