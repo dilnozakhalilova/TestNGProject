56a2301e4a32271ba5a6c6f8b0775de00892026c
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-02-10 21:49:31 -0500

    Data Provider

--- src/com/exelenter/class4/_02_DataProvider.java
+++ src/com/exelenter/class4/_02_DataProvider.java
@@ -1,5 +1,33 @@
 package com.exelenter.class4;
 
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class _02_DataProvider {
+
+    // DataProvider it works with 2D Array and accept Object[] as data type.
+@Test
+    void printData(){
+    userList();
+
+
+}
+public void userList(){
+    List<String> stringArrayList =new ArrayList<>();
+    stringArrayList.add("John");
+    stringArrayList.add("Sam");
+    // stringArrayList.add(25); gives error because it is a String
+    List<Object> objectList = new ArrayList<>();
+    objectList.add("John");
+    objectList.add(25);
+    System.out.println("stringArrayList = " + stringArrayList);
+    System.out.println("objectList = " + objectList);
+}
+
+
+
 
 }
