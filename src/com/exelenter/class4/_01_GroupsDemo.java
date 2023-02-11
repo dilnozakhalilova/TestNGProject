fceb8aa9f8494e452949cf72e18c2afb9f92278f
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-02-10 20:55:59 -0500

    Smoke test

--- src/com/exelenter/class4/_01_GroupsDemo.java
+++ src/com/exelenter/class4/_01_GroupsDemo.java
@@ -1,2 +1,26 @@
-package com.exelenter.class4;public class _01_GroupsDemo {
+package com.exelenter.class4;
+
+import org.testng.annotations.Test;
+
+public class _01_GroupsDemo {
+    @Test
+    void test1() {
+        System.out.println("Test 1");
+    }
+
+    @Test(groups = "smoke")
+    void test2() {
+        System.out.println("Test 2");
+    }
+
+    @Test(groups ="regression")
+    void test3() {
+        System.out.println("Test 3");
+    }
+
+    @Test(groups = {"smoke","regression"})
+    void test4() {
+        System.out.println("Test 4");
+    }
+
 }
