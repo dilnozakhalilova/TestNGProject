b683880276b786b32b4a4088aabd12705a1a452a
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-02-13 20:50:54 -0500

    new users

--- src/com/exelenter/class4/_04_DataProviderDemo3.java
+++ src/com/exelenter/class4/_04_DataProviderDemo3.java
@@ -1,55 +1,34 @@
 package com.exelenter.class4;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.ObjectFactory;
 import org.testng.annotations.Test;
 import utils.BaseClass;
 import utils.ConfigsReader;
 
 public class _04_DataProviderDemo3 extends BaseClass {
 
-    // Instead of repeating ourselves with many test methods (login1, login2,login3,etc) instead we can use DataProvider
+    // Instead of repeating ourselves with many test methods (login1, login2,login3,etc.) instead we can use DataProvider
 
-    @Test
-
-    public void loginTest(){
-        sendText(loginPage.username, ConfigsReader.getProperties("username"));
-        sendText(loginPage.password, ConfigsReader.getProperties("password"));
+    @Test(dataProvider = "dataProviderMethod")
+    public void loginTest(String username, String password) {
+        sendText(loginPage.username, username);
+        sendText(loginPage.password, password);
         clickButWaitForClickability(loginPage.loginBtn);
 
         boolean welcomeDisplayed = dashboardPage.welcome.isDisplayed();
         Assert.assertTrue(welcomeDisplayed, "Welcome message is not displayed");
     }
 
-    public void loginTest2(){
-        sendText(loginPage.username, ConfigsReader.getProperties("username123"));
-        sendText(loginPage.password, ConfigsReader.getProperties("password"));
-        clickButWaitForClickability(loginPage.loginBtn);
-
-        boolean welcomeDisplayed = dashboardPage.welcome.isDisplayed();
-        Assert.assertTrue(welcomeDisplayed, "Welcome message is not displayed");
+    @DataProvider
+    public Object [][] dataProviderMethod(){
+        Object [][]data = {
+                {"Admin","Exelent202"},
+                {"johndoe","k#G886@H"},
+                {"EssUser","Ess@2023"}
+        };
+        return data;
     }
-
-    public void loginTest3(){
-        sendText(loginPage.username, ConfigsReader.getProperties("username"));
-       // sendText(loginPage.password, ConfigsReader.getProperties("password"));
-        clickButWaitForClickability(loginPage.loginBtn);
-
-        boolean welcomeDisplayed = dashboardPage.welcome.isDisplayed();
-        Assert.assertTrue(welcomeDisplayed, "Welcome message is not displayed");
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
