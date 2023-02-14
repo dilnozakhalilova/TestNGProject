e83e3717a049429fefe5536e4d5345ec52442722
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-02-13 21:38:45 -0500

    Personal details

--- src/pages/PersonalDetailsPage.java
+++ src/pages/PersonalDetailsPage.java
@@ -1,6 +1,18 @@
 package pages;
 
-public class PersonalDetailsPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.BaseClass;
+
+public class PersonalDetailsPage extends BaseClass {
+    @FindBy(id="personal_txtEmployeeId")
+    public WebElement employeeId;
+
+
+    public PersonalDetailsPage(){
+        PageFactory.initElements(driver,this);
+    }
 
 
 }
