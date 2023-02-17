eb53a68f819fbcc22c3fcb22db6346006dc71b5f
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-02-16 22:07:47 -0500

    Updates

--- src/utils/excelUtility.java
+++ src/utils/excelUtility.java
@@ -1,24 +1,33 @@
 package utils;
 
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 // we need to import Apache POI libraries for this to work
 public class excelUtility {
     // Order: 1.filePath, 2.Workbook, 3.Sheet, 4.Rows & Col, 5.Cell
 
     static String projectPath = System.getProperty("user.dir");
+    FileInputStream fis;
+    Workbook workbook;
+    Sheet sheet;
+
     void getFilePath(String filePath) {
 
         try {
-            FileInputStream fis = new FileInputStream(filePath);
-            Workbook workbook = new XSSFWorkbook();
-        } catch (FileNotFoundException e) {
+            fis = new FileInputStream(filePath);
+            workbook = new XSSFWorkbook(fis);
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    void getSheet(String sheetName) {
+        sheet = workbook.getSheet(sheetName);
     }
 
 
